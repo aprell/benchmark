43b67fe899f93c834c4aa75b5b69527534c2838b
Andreas Prell <andreas.h.prell@gmail.com>
2022-05-23 22:59:55 +0200

    Import files

---
+++ benchmark.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+import argparse
+import glob
+import os
+import re
+import subprocess
+
+from stats import print_stats
+from testrun import eprint, testrun
+
+
+def get_num_threads(env):
+    """
+    Get the value of environment variable `env` if defined, else get the number of CPUs from `lscpu`
+    """
+    num_threads = os.environ.get(env)
+    if not num_threads:
+        num_threads = subprocess.check_output("lscpu | grep ^CPU\(s\)", shell=True).split()[1]
+    return int(num_threads)
+
+
+def get_logfile(cmd, suffix=None, ext=None):
+    logdir = os.path.join("benchmark.output", os.path.dirname(cmd[0]))
+    logfile = os.path.join(logdir, os.path.basename(cmd[0]))
+    if len(cmd) > 1:
+        logfile += "_" + "_".join(cmd[1:])
+    if suffix:
+        logfile += f"_{suffix:03}"
+    if ext:
+        logfile += f".{ext}"
+    return logfile
+
+
+def get_logfiles(cmd, ext=None):
+    def get_num_threads(logfile):
+        return int(os.path.splitext(logfile)[0][-3:])
+    pattern = get_logfile(cmd) + "*"
+    if ext:
+        pattern += f".{ext}"
+    logfiles = glob.glob(pattern)
+    return [(get_num_threads(logfile), logfile) for logfile in sorted(logfiles)]
+
+
+def get_run_times(logfile):
+    run_times = []
+    with open(logfile) as file:
+        elapsed = re.compile(r"[^:]*: (\d+(?:\.\d*)?)")
+        for line in file:
+            match = re.search(elapsed, line)
+            if match:
+                run_times.append(float(match.group(1)))
+    return run_times
+
+
+def benchmark(cmd, repetitions=10, show_statistics=False, env="OMP_NUM_THREADS"):
+    num_threads = get_num_threads(env)
+    logfile = get_logfile(cmd, suffix=num_threads, ext="log")
+    os.makedirs(os.path.dirname(logfile), exist_ok=True)
+
+    with open(logfile, "w") as file:
+        eprint(f"{env}={num_threads} ", end='')
+        testrun(cmd, repetitions, stdout=file)
+
+    if show_statistics:
+        print_stats(get_run_times(logfile))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-r", "--repetitions",
+                        type=int, default=10,
+                        help="number of repetitions (default is 10)",
+                        required=False)
+
+    parser.add_argument("-s", "--show-statistics",
+                        action="store_true",
+                        help="show summary statistics",
+                        required=False)
+
+    parser.add_argument("cmd",
+                        nargs="*",
+                        help="program to run")
+
+    args = parser.parse_args()
+
+    if args.cmd:
+        benchmark(args.cmd, args.repetitions, args.show_statistics)
+    else:
+        # Read commands from file
+        with open("benchmark.input") as file:
+            for line in file:
+                cmd = line.split()
+                benchmark(cmd, args.repetitions, args.show_statistics)
---
+++ stats.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+import argparse
+import statistics
+import sys
+
+
+def mean_rsd(numbers):
+    """
+    Calculate mean and relative standard deviation (RSD) of a series of numbers
+    """
+    if sys.version_info >= (3,8):
+        mean = statistics.fmean(numbers)
+    else:
+        mean = statistics.mean(numbers)
+    try:
+        sd = statistics.stdev(numbers)
+        rsd = 100 * sd / abs(mean)
+    except statistics.StatisticsError:
+        # Variance requires at least two data points
+        rsd = None
+    return mean, rsd
+
+
+def quartiles(numbers):
+    return statistics.quantiles(numbers, n=4, method="inclusive")
+
+
+def deciles(numbers):
+    return statistics.quantiles(numbers, n=10, method="inclusive")
+
+
+def format_number(n):
+    return format_mean_rsd(*n) if isinstance(n, tuple) else \
+        str(round(n, 2) if isinstance(n, float) else n)
+
+
+def format_mean_rsd(mean, rsd):
+    return f"{format_number(mean)} ± {format_number(rsd)} %"
+
+
+def print_csv(stats, file=sys.stdout):
+    print(",".join(stats[0]), file=file)
+    for i in range(1, len(stats)):
+        print(",".join(map(format_number, stats[i])), file=file)
+    file.flush()
+
+
+def print_table(stats, file=sys.stdout):
+    import tabulate as T
+    for i in range(1, len(stats)):
+        stats[i] = map(format_number, stats[i])
+    print(T.tabulate(stats, headers="firstrow", tablefmt="pretty"), file=file)
+    file.flush()
+
+
+if sys.version_info >= (3,8):
+    headers = [
+        "Min", "P10", "P25", "Median", "P75", "P90", "Max",
+        "P75-P25", "P90-P10", "Max-Min", "Mean ± RSD"
+    ]
+else:
+    # Fewer stats (no percentiles)
+    headers = ["Min", "Median", "Max", "Max-Min", "Mean ± RSD"]
+
+
+def summarize(numbers):
+    if not numbers:
+        return []
+
+    if sys.version_info >= (3,8):
+        try:
+            Q = quartiles(numbers)
+            D = deciles(numbers)
+        except statistics.StatisticsError:
+            # Must have at least two data points
+            Q = None
+            D = None
+
+        stats = [
+            min(numbers),                 # Minimum
+            D[0] if D else None,          # 10th percentile / 1st decile
+            Q[0] if Q else None,          # 25th percentile / 1st quartile
+            Q[1] if Q else None,          # 50th percentile / 2nd quartile / median
+            Q[-1] if Q else None,         # 75th percentile / 3rd quartile
+            D[-1] if D else None,         # 90th percentile / 9th decile
+            max(numbers),                 # Maximum
+            Q[-1] - Q[0] if Q else None,  # Interquartile range
+            D[-1] - D[0] if D else None,  # Interdecile range
+            max(numbers) - min(numbers),  # Total range
+            mean_rsd(numbers)             # Mean ± RSD in percent
+        ]
+    else:
+        stats = [
+            min(numbers),                 # Minimum
+            statistics.median(numbers),   # Median
+            max(numbers),                 # Maximum
+            max(numbers) - min(numbers),  # Total range
+            mean_rsd(numbers)             # Mean ± RSD in percent
+        ]
+
+    return stats
+
+
+def print_stats(numbers, tabulate=True):
+    stats = [headers, summarize(numbers)]
+    print_table(stats) if tabulate else print_csv(stats)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-t", "--tabulate",
+                        action="store_true",
+                        help="tabulate output",
+                        required=False)
+
+    args = parser.parse_args()
+
+    numbers = []
+    for line in sys.stdin:
+        numbers.append(float(line))
+
+    print_stats(numbers, args.tabulate)
