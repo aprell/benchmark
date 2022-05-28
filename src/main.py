067634de758c7a4c00475bdf93c5e331fb18b77f
Andreas Prell <andreas.h.prell@gmail.com>
2022-05-28 10:37:57 +0200

    Support plain, uncolored diffs

--- src/main.py
+++ src/main.py
@@ -114,22 +114,27 @@ def plot(cmds, outfile, xlabel="Number of threads", ylabel="Median run times (ms
     plt.savefig(outfile)
 
 
-def format_number(n, suffix=""):
-    return f"{BAD}+{n:.2f}{suffix}{RESET}" if n > 0 else f"{GOOD}{n:.2f}{suffix}{RESET}"
+def format_number(n, suffix="", color=True):
+    if color:
+        return f"{BAD}+{n:.2f}{suffix}{RESET}" if n > 0 else f"{GOOD}{n:.2f}{suffix}{RESET}"
+    else:
+        return f"+{n:.2f}{suffix}" if n > 0 else f"{n:.2f}{suffix}"
 
 
 def actual_diff(a, b):
-    return np.vectorize(lambda n: format_number(n, suffix=" ms"))(a - b)
+    return a - b
 
 
 def relative_diff(a, b):
-    return np.vectorize(lambda n: format_number(n, suffix=" %"))((a - b) / b * 100)
+    return (a - b) / b * 100
 
 
-def diff(cmds, func):
+def diff(cmds, func, unit, color=True):
+    def format(n):
+        return format_number(n, suffix=f" {unit}", color=color)
     a = np.array(get_stats(cmds[0]))
     b = np.array(get_stats(cmds[1]))
-    d = func(a[1:,1:-4].astype(float), b[1:,1:-4].astype(float))
+    d = np.vectorize(format)(func(a[1:,1:-4].astype(float), b[1:,1:-4].astype(float)))
     print("\n", cmds[0], "vs", cmds[1])
     print_table(np.r_[[a[0,:-4]], np.c_[a[1:,0], d]].tolist())
 
@@ -226,8 +231,8 @@ def main():
             plot(args.plot, outfile)
 
     if args.diff:
-        diff(args.diff, func=actual_diff)
-        diff(args.diff, func=relative_diff)
+        diff(args.diff, func=actual_diff, unit="ms")
+        diff(args.diff, func=relative_diff, unit="%")
 
 
 if __name__ == "__main__":
