4c24bed2ba77c26d5dcdd57ed25c9d5752856107
Andreas Prell <andreas.h.prell@gmail.com>
2024-01-09 18:03:37 +0100

    Add option to specify a config file

--- bench/main.py
+++ bench/main.py
@@ -7,13 +7,14 @@ from bench import config, diff, plot, report, run, test
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", metavar="FILE", help="use FILE as config file", required=False)
     subparsers = parser.add_subparsers()
 
     for cmd in [test, run, report, diff, plot]:
         cmd.setup(subparsers)
 
     args = parser.parse_args()
-    args.run(args, config.Config("bench.ini"))
+    args.run(args, config.Config(args.file or "bench.ini"))
 
 
 if __name__ == "__main__":
